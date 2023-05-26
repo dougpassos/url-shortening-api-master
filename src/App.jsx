0a52e6144496fded580fc71cfcb6cc8dd4c7a529
Douglas Passos <douglas.p.doug@gmail.com>
2023-05-26 18:53:22 -0300

    preparar proj para React

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <header>Header</header>
+     <div>Area grande - MOre than just short links</div>
+     <div>Encurtador de URL</div>
+     <div>Advanced Statistics</div>
+     <div>Cards com informações</div>
+     <div>Faixa com botão Get Started</div>
+     <footer>Footer</footer>
+    </>
+  )
+}
+
+export default App
