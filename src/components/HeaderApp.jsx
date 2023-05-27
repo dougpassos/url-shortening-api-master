fdb2719443c09651cbfee93882917e17e098ac50
Douglas Passos <douglas.p.doug@gmail.com>
2023-05-27 18:50:30 -0300

    criar component e layout

--- src/components/HeaderApp.jsx
+++ src/components/HeaderApp.jsx
@@ -1,12 +1,23 @@
-
+import './HeaderApp.css'
+import imgLogo from '../../public/logo.svg'
 
 function HeaderApp () {
   return (
     <>
-      <div>
-        <p>DOUGLAS</p>
-
-      </div>
+      <nav>
+        <img src={imgLogo} alt="logo" />
+        <div className='menu'>
+          <ul>
+            <li><a href="#">Feature</a></li>
+            <li><a href="#">Pricing</a></li>
+            <li><a href="#">Resources</a></li>
+          </ul>
+          <div className='bt-login'>
+            <button>Login</button>
+            <button>Sign Up</button>
+          </div>
+        </div>
+      </nav>
     </>
   )
 }
