3b3d42fc0f4777a649292c838b35b622bb8e3d29
Douglas Passos <douglas.p.doug@gmail.com>
2023-05-30 16:50:36 -0300

    component buttonGetStarted

---
+++ src/components/ButtonGetStarted.jsx
@@ -0,0 +1,13 @@
+import './ButtonGetStarted.css'
+
+function ButtonGetStarted() {
+
+  return (
+    <>  
+      <button className='get-started'>Get Started</button>      
+    </>
+
+  )  
+}
+
+export default ButtonGetStarted;
